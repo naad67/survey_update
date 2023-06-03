65571a9055275f38aed66c847aa6e678ff5a498d
adil <naad7182@gmaiil.com>
2023-06-03 14:08:14 +0600

    update

--- __manifest__.py
+++ __manifest__.py
@@ -1,18 +1,19 @@
 {
-    'name': "Survey Update",
+    'name': "Culture Analyzer",
     'summary': """
-        Qzhub Assets module adding new functionality to existing asset for AstanaREC project""",
+        Culture Analyzer Project with Denison model""",
 
     'description': """
-        Qzhub Assets module adding new functionality to existing asset for AstanaREC project
+        Culture Analyzer Project with Denison model
     """,
 
-    'author': "TOO QZhub",
-    'website': "https://www.qzhub.com",
+    'author': "Adil Nabiola",
+    'website': "https://github.com/naad67",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
+    'licence': "LGPL-3",
     'category': 'Technical Settings',
     'version': '15.0',
     # any module necessary for this one to work correctly
